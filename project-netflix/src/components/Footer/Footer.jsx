5db578e24186ac7ea5972f811d73aa40561f941e
adan-dev-web <adan.nancel@epitech.eu>
2025-09-17 23:27:59 +0200

    Footer + login done

--- project-netflix/src/components/Footer/Footer.jsx
+++ project-netflix/src/components/Footer/Footer.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./Footer.css";
+import youtube_icon from "../../assets/youtube_icon.png";
+import twitter_icon from "../../assets/twitter_icon.png";
+import instagram_icon from "../../assets/instagram_icon.png";
+import facebook_icon from "../../assets/facebook_icon.png";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer-icons">
+        <img src={youtube_icon} alt="Footer icons" />
+        <img src={twitter_icon} alt="Footer icons" />
+        <img src={instagram_icon} alt="Footer icons" />
+        <img src={facebook_icon} alt="Footer icons" />
+      </div>
+      <ul>
+        <li>Audio description</li>
+        <li>Centre d'aide</li>
+        <li>Cartes cadeaux</li>
+        <li>Centre de media</li>
+        <li>Relations avec les investisseurs</li>
+        <li>Conditions d'utilisation</li>
+        <li>Politique de confidentialité</li>
+        <li>Notions légales</li>
+        <li>Préférences de cookies</li>
+        <li>Informations sur les publicités</li>
+        <li>Contactez-nous</li>
+      </ul>
+      <p className="copyright-text">
+        © 2025 Netflix Clone by AdanDev. Tous droits réservés.
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- project-netflix/src/pages/Login/Login.jsx
+++ project-netflix/src/pages/Login/Login.jsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+import logo from "../../assets/logo.png";
 
 const Login = () => {
-  return <div className="login">Login</div>;
+  const [signState, setSignState] = useState("Connexion");
+
+  return (
+    <div className="login">
+      <img src={logo} className="login-logo" alt="Logo" />
+      <div className="login-form">
+        <h1>{signState}</h1>
+        <form>
+          {signState === "Inscription" ? (
+            <input type="text" placeholder="Pseudo" />
+          ) : (
+            <></>
+          )}
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Mot de passe" />
+          <button>{signState}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Se souvenir de moi</label>
+            </div>
+            <p>Besoin d'aide ?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          {signState === "Connexion" ? (
+            <p>
+              Nouveau sur Netflix ?{" "}
+              <span onClick={() => setSignState("Inscription")}>
+                Inscrivez-vous
+              </span>
+            </p>
+          ) : (
+            <p>
+              Déjà un compte ?{" "}
+              <span onClick={() => setSignState("Connexion")}>
+                Connectez-vous
+              </span>
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
